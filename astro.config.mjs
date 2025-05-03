8a79da7030b24e5d0343400da23f3f7a6739db7a
trafficlunar <hello@trafficlunar.net>
2025-05-03 18:11:20 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-
-	integrations: [svelte()],
+	site: "https://trafficlunar.net",
+	integrations: [svelte(), sitemap()],
 });
