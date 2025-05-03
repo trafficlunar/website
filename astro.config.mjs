2722a7370be3b9d1a474f944779014f5ab45c0cd
trafficlunar <hello@trafficlunar.net>
2025-05-03 19:40:05 +0100

    feat: nekoweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { env } from "node:process";
+import { loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
+import nekoweb from "@indiefellas/astro-adapter-nekoweb";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
+
+const { NEKOWEB_API_KEY, NEKOWEB_COOKIE } = loadEnv(env.NODE_ENV ?? "", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +16,10 @@ export default defineConfig({
 		plugins: [tailwindcss()],
 	},
 	site: "https://trafficlunar.net",
+	adapter: nekoweb({
+		apiKey: NEKOWEB_API_KEY,
+		cookie: NEKOWEB_COOKIE,
+		folder: "dist",
+	}),
 	integrations: [svelte(), sitemap()],
 });
