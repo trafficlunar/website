7c2c4ffc503efbd297ded5ce98c2e44fec4cc702
trafficlunar <hello@trafficlunar.net>
2025-11-29 14:57:55 +0000

    feat: truncate song text

--- src/components/Music.svelte
+++ src/components/Music.svelte
@@ -40,7 +40,7 @@
 				<img src={image} alt="album cover" class="size-20 rounded-full animate-spin animate-duration-30s" />
 
 				<div class="flex flex-col w-full min-w-0">
-					<h1 class="font-medium text-lg">{song}</h1>
+					<h1 class="font-medium text-lg truncate">{song}</h1>
 					<span class="text-sm">{artist}</span>
 				</div>
 			</div>
@@ -50,7 +50,7 @@
 				width="22"
 				height="22"
 				viewBox="0 0 20 20"
-				class="absolute right-1 top-1 z-10 text-overlay1 transition-all group-hover:top-0.5 group-hover:right-0.5 group-hover:text-peach"
+				class="absolute right-1 top-1 z-10 text-overlay1 transition-all duration-200 group-hover:top-0.5 group-hover:right-0.5 group-hover:text-peach"
 				><path
 					fill="currentColor"
 					d="M6.25 4.5A1.75 1.75 0 0 0 4.5 6.25v7.5c0 .966.784 1.75 1.75 1.75h7.5a1.75 1.75 0 0 0 1.75-1.75v-2a.75.75 0 0 1 1.5 0v2A3.25 3.25 0 0 1 13.75 17h-7.5A3.25 3.25 0 0 1 3 13.75v-7.5A3.25 3.25 0 0 1 6.25 3h2a.75.75 0 0 1 0 1.5zm4.25-.75a.75.75 0 0 1 .75-.75h5a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0V5.56l-3.72 3.72a.75.75 0 1 1-1.06-1.06l3.72-3.72h-3.19a.75.75 0 0 1-.75-.75"
